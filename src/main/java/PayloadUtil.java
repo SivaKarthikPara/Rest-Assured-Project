855e0fd14b626a16d49f9b462fc4837cc2a1fc13
Siva Karthik Para <siva.karthik.p@oracle.com>
2022-10-09 19:15:26 +0530

    Adding rest assured demo project

---
+++ src/main/java/PayloadUtil.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class PayloadUtil {
+
+    public static String addBookBody(Map<String, String> bookData) {
+        String book = "{\n" +
+                "    \"name\": \"" + bookData.get("name") + "\",\n" +
+                "    \"isbn\": \"" + bookData.get("isbn") + "\",\n" +
+                "    \"aisle\": \"" + bookData.get("aisle") + "\",\n" +
+                "    \"author\": \"" + bookData.get("author") + "\"\n" +
+                "}";
+        return book;
+    }
+
+    public static String addPlaceBody() {
+        return "{\n" +
+                "  \"location\": {\n" +
+                "    \"lat\": -38.383494,\n" +
+                "    \"lng\": 33.427362\n" +
+                "  },\n" +
+                "  \"accuracy\": 50,\n" +
+                "  \"name\": \"Frontline house\",\n" +
+                "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                "  \"address\": \"29, side layout, cohen 09\",\n" +
+                "  \"types\": [\n" +
+                "    \"shoe park\",\n" +
+                "    \"shop\"\n" +
+                "  ],\n" +
+                "  \"website\": \"http://google.com\",\n" +
+                "  \"language\": \"French-IN\"\n" +
+                "}";
+    }
+
+    public static String updatePlaceBody(String placeId) {
+        return "{\n" +
+                "    \"place_id\": \"" + placeId + "\",\n" +
+                "    \"address\": \"Hyderabad, India\",\n" +
+                "    \"key\": \"qaclick123\"\n" +
+                "}";
+    }
+
+    public static String deletePlaceBody(String placeId) {
+        return "{\n" +
+                "    \"place_id\": \"" + placeId + "\"\n" +
+                "}";
+    }
+
+    public static String demoJsonArray() {
+        try {
+            InputStream inputStream = PayloadUtil.class.getClassLoader().getResourceAsStream("json.models/jsonpathdemo.json");
+            String json = new BufferedReader(
+                    new InputStreamReader(inputStream))
+                    .lines()
+                    .collect(Collectors.joining("\n"));
+            return json;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "{ \"msg\" : \"No Json\"}";
+    }
+}
